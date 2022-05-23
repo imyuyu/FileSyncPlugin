c86d06149749beb0d264b3482314a0957a268b1b
GitHub Action <action@github.com>
2022-05-23 14:09:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/imyuyu/filesyncplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/imyuyu/filesyncplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.imyuyu.filesyncplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imyuyu.filesyncplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/imyuyu/filesyncplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/imyuyu/filesyncplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.imyuyu.filesyncplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imyuyu.filesyncplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
